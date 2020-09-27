9eac9525e247e04ea60bc15fb19dddfb8184e9ee
MaRaka0 <71791228+MaRaka0@users.noreply.github.com>
2020-09-27 17:27:18 +0300

    Update app.js

--- blog/resources/js/app.js
+++ blog/resources/js/app.js
@@ -1,9 +1,28 @@
 require('./bootstrap');
 let page = document.querySelector('.page');
-let themeButton = document.querySelector('.theme-button');
+        let themeButton = document.querySelector('.theme-button');
 
-themeButton.onclick = function() {
-    page.classList.toggle('light-theme');
-    page.classList.toggle('dark-theme');
-};
+        themeButton.onclick = function () {
+            page.classList.toggle('light-theme');
+            page.classList.toggle('dark-theme');
+        };
 
+        let upButton = document.querySelector('.up-button');
+
+        window.onscroll = function () {
+            if (window.pageYOffset > 200) {
+                upButton.classList.add('shown');
+            } else {
+                upButton.classList.remove('shown');
+            }
+        };
+
+        upButton.onclick = function () {
+            window.scrollTo(0, 0);
+        };
+
+        let downButton = document.querySelector('.down-button')
+
+        downButton.onclick = function() {
+            window.scrollTo(0, 1000000000);
+        }
