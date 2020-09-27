189afda827be043f82526f998d29a7b464b09315
MaRaka0 <71791228+MaRaka0@users.noreply.github.com>
2020-09-27 17:24:17 +0300

    Update app.blade.php

--- blog/resources/views/layouts/app.blade.php
+++ blog/resources/views/layouts/app.blade.php
@@ -8,8 +8,9 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}" >
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 </head>
-<body>
+<body class="page light-theme transition-fade" id="swup">
     @include('inc.header')
 
     <div class="container">
@@ -25,6 +26,39 @@
         </div>
     </div>
     @include('inc.footer')
+    <button class="up-button" type="button">
+        
+        <span class="visually-hidden"><img src="https://img.icons8.com/fluent/48/000000/up.png"/></span>
+    </button>
+    <script>
 
+        let page = document.querySelector('.page');
+        let themeButton = document.querySelector('.theme-button');
+
+        themeButton.onclick = function () {
+            page.classList.toggle('light-theme');
+            page.classList.toggle('dark-theme');
+        };
+
+        let upButton = document.querySelector('.up-button');
+
+        window.onscroll = function () {
+            if (window.pageYOffset > 200) {
+                upButton.classList.add('shown');
+            } else {
+                upButton.classList.remove('shown');
+            }
+        };
+
+        upButton.onclick = function () {
+            window.scrollTo(0, 0);
+        };
+
+        let downButton = document.querySelector('.down-button')
+
+        downButton.onclick = function() {
+            window.scrollTo(0, 1000000000);
+        }
+    </script>
 </body>
 </html>
